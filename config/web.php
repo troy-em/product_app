b515502bf7f91a37ba617cc77bb7c1e9127d2b6a
troy-em <troy.em@probt.tech>
2023-01-14 03:10:12 +0300

    addproduct page

--- config/web.php
+++ config/web.php
@@ -43,14 +43,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'addproduct' => 'addproduct/index'
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionAddproduct()
+    {
+        return $this->render('addproduct');
+    }
 }
