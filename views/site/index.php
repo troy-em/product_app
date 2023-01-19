dbf6c404c53dfef7cf8533d9950475b7060c8ff3
troy-em <troy.em@probt.tech>
2023-01-19 12:34:53 +0300

    display

--- views/site/index.php
+++ views/site/index.php
@@ -11,21 +11,22 @@ use yii\bootstrap5\Html;
         <h1 class="display-4">SNEAKERZ</h1>
 
         <p class="lead">Check Out Our Products</p>
+        <h2 class="title  text-center">Featured Items</h2>
 
-        <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
     <div class="col-sm-9 padding-right">
         <div class="features_items">
-            <h2 class="title  text-center">Featured Items</h2>
             <?php
              foreach($products as $product) {
             ?>
-            <div class="col-sm-4">
-                <div class="product-image-wrapper">
+            <div class="col-sm-4" style="margin: 1.5em;">
+                <div class="product-image-wrapper" >
                     <div class="single-products">
                         <div class="productinfo text-center">
-                        <?= Html::img("uploads/{$product->image}", ['alt' => $product->name]) ?>
-                            <h2>$ <?= $product->selling_price?></h2>
+                        <?= Html::img("uploads/{$product->image}", ['alt' => $product->name, 'height' => '300em']) ?>
+                        <br>
+                        <br>
+                        <h2>$ <?= $product->selling_price?></h2>
                             <p><?= $product->name?></p>
                             <a href="http://" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                         </div>
@@ -48,42 +49,5 @@ use yii\bootstrap5\Html;
              }
             ?>
         </div>
-
     </div>
-<!--
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-    </div> -->
 </div>
